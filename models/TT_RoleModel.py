1ba5b438876e661c346648e331df21cb7896d9d5
Prajapati Ansh <prajapatiansh137@gmail.com>
2025-03-10 12:19:52 +0530

    New APIs created

--- models/TT_RoleModel.py
+++ models/TT_RoleModel.py
@@ -7,10 +7,24 @@ class UserSignup(BaseModel):
     password:str
     role:str
     
+class UserOut(UserSignup):
+        id:str = Field(alias="_id") 
+        role:Optional[str] = None
+        email:Optional[str] = None
+        password:Optional[str] = None
+
+        @validator("id",pre=True,always=True)
+        def convert_id(cls,v):
+            if isinstance(v,ObjectId):
+                return str(v)
+            return v
+        
 
 class UserLogin(BaseModel):
     username:str
     password:str
+
+    
 
 
 
